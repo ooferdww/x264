3b8b7574cb9d6f1bec2fd4ec151c1a423df08931
ooferdww <91383616+ooferdww@users.noreply.github.com>
2023-06-03 17:06:07 +0300

    damage control

--- encoder/set.c
+++ encoder/set.c
@@ -607,7 +607,9 @@ int x264_sei_version_write( x264_t *h, bs_t *s )
     CHECKED_MALLOC( payload, 200 + strlen( opts ) );
 
     memcpy( payload, uuid, 16 );
-    sprintf();
+    sprintf( payload, "x264 - core %d%s - H.264/MPEG-4 AVC codec - "
+             "Copy%s 2003-2022 - http://www.videolan.org/x264.html",
+             X264_BUILD, X264_VERSION, HAVE_GPL?"left":"right", opts );
     length = strlen(payload)+1;
 
     x264_sei_write( s, (uint8_t *)payload, length, SEI_USER_DATA_UNREGISTERED );
